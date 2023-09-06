3e41736d9609664bb629aabfb8a5927a5cfac732
IJTINK7 <ijtink7@mail.ru>
2023-09-06 15:02:53 +0300

    Adding data types

---
+++ 01/01.js
@@ -0,0 +1,12 @@
+// 1-st lesson
+// I - Типы данных:
+//  Примитивы:
+//      1) number
+//      2) string
+//      3) boolean
+//      4) BigInt
+//      5) null
+//      6) undefined
+//      7) symbol
+//  Ссылочный:
+//      1) object (objects, arrays, functions, etc.)
