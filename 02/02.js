d61267d4c0327e7b83b2e7cdd9eac9415cdb01f3
IJTINK7 <ijtink7@mail.ru>
2023-09-20 16:05:49 +0300

    Changing custom "pop" arrays method

--- 02/02.js
+++ 02/02.js
@@ -31,44 +31,44 @@ const users = [
 // Custom "map" arrays method
 const getNames = (array) => {
     const newArray = []
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         newArray.push(array[i].name)
     }
     return newArray
 }
 console.log(getNames(users))
 
-const addScores = (array) =>{
+const addScores = (array) => {
     const result = []
-    const getNewScoresForResult = (el)=>({...el, scores: + el.scores + 5 })
-    for(let i = 0; i < array.length; i++){
+    const getNewScoresForResult = (el) => ({...el, scores: +el.scores + 5})
+    for (let i = 0; i < array.length; i++) {
         result[i] = getNewScoresForResult(array[i])
     }
     return result
 }
 console.log(addScores(users))
 
-const getMappedArray = (array, mapFunction) =>{
+const getMappedArray = (array, mapFunction) => {
     const result = []
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         result[i] = mapFunction(array[i])
     }
     return result
 }
-console.log(getMappedArray(users, el=> ({...el, isStudent: !el.isStudent})))
-console.log(getMappedArray(users, el=> ({...el, scores: el.scores + 7})))
-console.log(getMappedArray(users, el=> ({...el, name: "Name: " + el.name})))
+console.log(getMappedArray(users, el => ({...el, isStudent: !el.isStudent})))
+console.log(getMappedArray(users, el => ({...el, scores: el.scores + 7})))
+console.log(getMappedArray(users, el => ({...el, name: "Name: " + el.name})))
 
 // using "map" arrays method
-console.log(users.map(el=>({...el, scores: el.scores +7})))
+console.log(users.map(el => ({...el, scores: el.scores + 7})))
 
 
 // FILTER
 
-const getFilteredArray = (array) =>{
+const getFilteredArray = (array) => {
     const result = []
-    for(let i = 0; i < array.length; i++){
-        if(array[i].scores > 80){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].scores > 80) {
             result.push(array[i])
         }
     }
@@ -78,37 +78,45 @@ const getFilteredArray = (array) =>{
 console.log(getFilteredArray(users))
 
 
-
-const getFilteredArray2 = (array, filterFunction) =>{
+const getFilteredArray2 = (array, filterFunction) => {
     const result = []
-    for(let i = 0; i < array.length; i++){
-        if(filterFunction(array[i]) === true){
+    for (let i = 0; i < array.length; i++) {
+        if (filterFunction(array[i]) === true) {
             result.push(array[i])
         }
     }
     return result
 }
-console.log(getFilteredArray2(users, el=> el.scores > 30))
+console.log(getFilteredArray2(users, el => el.scores > 30))
 
 // FIND
 
 const findElement = (array, findFunction) => {
-    for(let i = 0; i < array.length; i++){
-        if(findFunction(array[i]) === true){
+    for (let i = 0; i < array.length; i++) {
+        if (findFunction(array[i]) === true) {
             return array[i]
         }
     }
     return array
 }
 
-console.log(findElement(users, el=>el.name === "Donald"))
-console.log(findElement(users, el=>el.name === "Roma"))
+console.log(findElement(users, el => el.name === "Donald"))
+console.log(findElement(users, el => el.name === "Roma"))
 
 // PUSH
+//
+// const createCustomPushMethod = (array, newItem) => {
+//     array[array.length] = newItem
+//     return array
+// }
+//
+// console.log(createCustomPushMethod(users, {id: 5, name: "Mike", isStudent: false, scores: 99}))
 
-const createCustomPushMethod = (array, newItem) => {
-    array[array.length] = newItem
+// POP
+
+const createCustomPopMethod = (array) => {
+    array.length = array.length - 1
     return array
 }
 
-console.log(createCustomPushMethod(users, {id: 5, name: "Mike", isStudent: false, scores: 99}))
+console.log(createCustomPopMethod(users))
