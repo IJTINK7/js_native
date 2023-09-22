1852e3e2b123db1a49b1950d26c947f1cb4d1ddf
IJTINK7 <ijtink7@mail.ru>
2023-09-22 19:50:39 +0300

    Adding some events info

---
+++ 03/03.js
@@ -0,0 +1,12 @@
+// function => handler(обработчик), listener(слушатель), subscriber (подписчик)
+// При наступлении события:
+// new Event() - объект, который содержит сведения о событии
+// function(event) - вызов функции
+// function(ev) - вызов функции
+// function(e) - вызов функции
+// event, ev,e === new Event()
+
+// [] === new Array()
+// {} === new Object()
+// const map = new Map()
+// const set = new Set()
