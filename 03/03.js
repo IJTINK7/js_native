579444c6eb2efb631a4cefc2acf66fab61def7d9
IJTINK7 <ijtink7@mail.ru>
2023-09-22 23:05:11 +0300

    Checking bubbling on elements (using TARGET)

--- 03/03.js
+++ 03/03.js
@@ -10,23 +10,23 @@
 // {} === new Object()
 // const map = new Map()
 // const set = new Set()
-
-const small = document.getElementById("small")
-const medium = document.getElementById("medium")
-const big = document.getElementById("big")
-const getSmallHandler = () => {
-    alert("Small!")
-}
-const getMediumHandler = () => {
-    alert("Medium!")
-}
-const getBigHandler = (e) => {
-    console.log(e.target.id)
-}
-// const anonymous = () => getSmallHandler("Hello")
-// small.onclick = anonymous
 //
-// small.onclick = null
-small.addEventListener("click",getBigHandler)
-medium.addEventListener("click",getBigHandler)
-big.addEventListener("click",getBigHandler)
+// const small = document.getElementById("small")
+// const medium = document.getElementById("medium")
+// const big = document.getElementById("big")
+// const getSmallHandler = () => {
+//     alert("Small!")
+// }
+// const getMediumHandler = () => {
+//     alert("Medium!")
+// }
+// const getBigHandler = (e) => {
+//     console.log(e.target.id)
+// }
+// // const anonymous = () => getSmallHandler("Hello")
+// // small.onclick = anonymous
+// //
+// // small.onclick = null
+// small.addEventListener("click",getBigHandler)
+// medium.addEventListener("click",getBigHandler)
+// big.addEventListener("click",getBigHandler)
