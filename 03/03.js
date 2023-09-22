0a1b0b7834f0764617acb248b609677dbe5f65ac
IJTINK7 <ijtink7@mail.ru>
2023-09-22 21:30:16 +0300

    Using "addEventListener" event listener

--- 03/03.js
+++ 03/03.js
@@ -11,11 +11,22 @@
 // const map = new Map()
 // const set = new Set()
 
-// const small = document.getElementById("small")
-// const getSmallHandler = (text) => {
-//     alert(text)
-// }
+const small = document.getElementById("small")
+const medium = document.getElementById("medium")
+const big = document.getElementById("big")
+const getSmallHandler = () => {
+    alert("Small!")
+}
+const getMediumHandler = () => {
+    alert("Medium!")
+}
+const getBigHandler = () => {
+    alert("Big!")
+}
 // const anonymous = () => getSmallHandler("Hello")
 // small.onclick = anonymous
 //
 // small.onclick = null
+small.addEventListener("click",getSmallHandler)
+medium.addEventListener("click",getMediumHandler)
+big.addEventListener("click",getBigHandler)
