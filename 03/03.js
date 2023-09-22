d1fa42fab5b73d211e3d5bf8137e3cf6a94111fc
IJTINK7 <ijtink7@mail.ru>
2023-09-22 21:17:11 +0300

    Using "onclick" event listener

--- 03/03.js
+++ 03/03.js
@@ -10,3 +10,12 @@
 // {} === new Object()
 // const map = new Map()
 // const set = new Set()
+
+// const small = document.getElementById("small")
+// const getSmallHandler = (text) => {
+//     alert(text)
+// }
+// const anonymous = () => getSmallHandler("Hello")
+// small.onclick = anonymous
+//
+// small.onclick = null
