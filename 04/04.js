3336179ab1aa28baf228f08aeaf99885db23a578
IJTINK7 <ijtink7@mail.ru>
2023-09-26 15:29:46 +0300

    Getting access to tasks via square bracket notation

--- 04/04.js
+++ 04/04.js
@@ -1,25 +1,28 @@
+const todolistId_1 = "24jh-672b-156b-009f"
+const todolistId_2 = "11gb-322b-121b-223f"
+
 const todolists = [
     {
-        todolistId: "24jh-672b-156b-009f",
+        todolistId: todolistId_1,
         title: "What to learn",
         filter: "All",
     },
     {
-        todolistId: "11gb-322b-121b-223f",
+        todolistId: todolistId_2  ,
         title: "What to buy",
         filter: "All",
     },
 ]
-
+console.log(todolists[0].todolistId)
 const tasks = {
-    "24jh-672b-156b-009f": [
+    [todolistId_1] : [
         {taskId: 0, title: "HTML", isDone: false},
         {taskId: 1, title: "CSS", isDone: false},
         {taskId: 2, title: "JS", isDone: true},
         {taskId: 3, title: "TS", isDone: true},
         {taskId: 4, title: "React", isDone: true}
-    ],
-    "11gb-322b-121b-223f": [
+        ],
+    [todolistId_2] : [
         {taskId: 5, title: "Milk", isDone: true},
         {taskId: 6, title: "Bread", isDone: true},
         {taskId: 7, title: "Eggs", isDone: false},
@@ -27,3 +30,6 @@ const tasks = {
         {taskId: 9, title: "Nuts", isDone: false}
     ],
 }
+console.log(tasks[todolistId_1])
+console.log(tasks["24jh-672b-156b-009f"])
+console.log(tasks[todolists[0].todolistId])
