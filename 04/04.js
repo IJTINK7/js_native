a34056ac79d9b93c23a26999e48c616d5a4a470f
IJTINK7 <ijtink7@mail.ru>
2023-09-26 12:39:35 +0300

    Creating "tasks" variable

--- 04/04.js
+++ 04/04.js
@@ -1,26 +1,29 @@
 const todolists = [
     {
-        todolistId: "0",
+        todolistId: "24jh-672b-156b-009f",
         title: "What to learn",
         filter: "All",
-        tasks:[
-            {taskId: 0, title: "HTML", isDone: false},
-            {taskId: 1, title: "CSS", isDone: false},
-            {taskId: 2, title: "JS", isDone: true},
-            {taskId: 3, title: "TS", isDone: true},
-            {taskId: 4, title: "React", isDone: true}
-        ]
     },
     {
-        todolistId: "1",
+        todolistId: "11gb-322b-121b-223f",
         title: "What to buy",
         filter: "All",
-        tasks:[
-            {taskId: 5, title: "Milk", isDone: true},
-            {taskId: 6, title: "Bread", isDone: true},
-            {taskId: 7, title: "Eggs", isDone: false},
-            {taskId: 8, title: "Water", isDone: false},
-            {taskId: 9, title: "Nuts", isDone: false}
-        ]
     },
 ]
+
+const tasks = {
+    "24jh-672b-156b-009f": [
+        {taskId: 0, title: "HTML", isDone: false},
+        {taskId: 1, title: "CSS", isDone: false},
+        {taskId: 2, title: "JS", isDone: true},
+        {taskId: 3, title: "TS", isDone: true},
+        {taskId: 4, title: "React", isDone: true}
+    ],
+    "11gb-322b-121b-223f": [
+        {taskId: 5, title: "Milk", isDone: true},
+        {taskId: 6, title: "Bread", isDone: true},
+        {taskId: 7, title: "Eggs", isDone: false},
+        {taskId: 8, title: "Water", isDone: false},
+        {taskId: 9, title: "Nuts", isDone: false}
+    ],
+}
