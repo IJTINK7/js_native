3febe2f82d67a1c6bea71fa0e6581185c72800ba
IJTINK7 <ijtink7@mail.ru>
2023-09-26 12:32:57 +0300

    Creating "todolists" variable

---
+++ 04/04.js
@@ -0,0 +1,26 @@
+const todolists = [
+    {
+        todolistId: "0",
+        title: "What to learn",
+        filter: "All",
+        tasks:[
+            {taskId: 0, title: "HTML", isDone: false},
+            {taskId: 1, title: "CSS", isDone: false},
+            {taskId: 3, title: "JS", isDone: true},
+            {taskId: 4, title: "TS", isDone: true},
+            {taskId: 5, title: "React", isDone: true}
+        ]
+    },
+    {
+        todolistId: "1",
+        title: "What to buy",
+        filter: "All",
+        tasks:[
+            {taskId: 0, title: "Milk", isDone: true},
+            {taskId: 1, title: "Bread", isDone: true},
+            {taskId: 3, title: "Eggs", isDone: false},
+            {taskId: 4, title: "Water", isDone: false},
+            {taskId: 5, title: "Nuts", isDone: false}
+        ]
+    },
+]
