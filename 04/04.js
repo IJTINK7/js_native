dbf6f067dabc6a0241db0279095462f4f651a605
IJTINK7 <ijtink7@mail.ru>
2023-09-26 15:55:38 +0300

    Finishing 04 lesson

--- 04/04.js
+++ 04/04.js
@@ -1,7 +1,7 @@
 const todolistId_1 = "24jh-672b-156b-009f"
 const todolistId_2 = "11gb-322b-121b-223f"
 
-const todolists = [
+const [todolists,setTodolists] = [
     {
         todolistId: todolistId_1,
         title: "What to learn",
@@ -11,7 +11,7 @@ const todolists = [
         todolistId: todolistId_2  ,
         title: "What to buy",
         filter: "All",
-    },
+    }
 ]
 console.log(todolists[0].todolistId)
 const tasks = {
@@ -33,3 +33,24 @@ const tasks = {
 console.log(tasks[todolistId_1])
 console.log(tasks["24jh-672b-156b-009f"])
 console.log(tasks[todolists[0].todolistId])
+
+// Код ниже не будет работать из-за отсутствия подключенного useState c функцией setTasks
+
+// const addTodolist = (newTitle) =>{
+//     const todolistId = "1243-12-15-235-e3456-1214"
+//     const newTodolist = {todolistId, title: newTitle, filter: "All"}
+//     setTodolists([...todolists, newTodolist])
+//     setTasks({...tasks, [todolistId]:[]})
+// }
+// const changeStatus = () => {
+//     setTasks({...tasks, [todolistId_1]: tasks[todolistId_1].map(el => el.id === 3 ? {...el, isDone: false}: el )})
+// }
+//
+// const deleteTask = (todoId, taskId) => {
+//     const todoTasks = tasks[todoId]
+//     const updatedTask = todoTasks.filter(el => el.id !== taskId )
+//     const copyTasks = {...tasks, [todoId]: updatedTask}
+// }
+// const deleteTask2 = (todoId, taskId) => {
+//     setTasks({...tasks, [todoId]: tasks[todoId].filter(el=>el.id !== taskId)})
+// }
