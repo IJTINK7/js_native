15ef96cd16ce245a4ce4ca0fc3c119dadc0228ac
IJTINK7 <ijtink7@mail.ru>
2024-01-16 19:54:38 +0300

    Adding files for using OOP info

---
+++ OOP/OOP.ts
@@ -0,0 +1,8 @@
+// class User {
+//     constructor(name: string, age: number) {
+//         this.name = "Roma"
+//         this.age = 28
+//     }
+// }
+
+// Модификаторы: public, protected, private, readonly
